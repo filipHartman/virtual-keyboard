83628682968aafc30941992f5b853eb64645054a
filipHartman <filip.hartman23@gmail.com>
2020-06-18 22:15:20 +0200

    feat: implement virtual keyboard app

---
+++ src/js/key.js
@@ -0,0 +1,71 @@
+import { modifyingKeys } from './config.js';
+import { getTextArea } from './utils.js';
+
+export function VirtualKeyBuilder() {
+  return {
+    setCode: function (code) {
+      this.code = code;
+      return this;
+    },
+
+    setDisplayValue: function (displayValue) {
+      this.displayValue = displayValue;
+      return this;
+    },
+
+    setAlternativeCode: function (alternativeCode) {
+      this.alternativeCode = alternativeCode;
+      return this;
+    },
+
+    build: function () {
+      return new VirtualKey(this.code, this.displayValue, this.alternativeCode);
+    },
+  };
+}
+
+class VirtualKey extends HTMLButtonElement {
+  constructor(code, displayValue = '', alternativeCode = '') {
+    super();
+    this.code = code;
+    this.displayValue = displayValue;
+    this.alternativeCode = alternativeCode;
+    this.className = 'key';
+    this.fillWithSymbol();
+  }
+
+  pressKey() {
+    this.classList.add('pressed');
+
+    if (
+      modifyingKeys.includes(this.code.toLowerCase()) ||
+      this.code === 'Space'
+    ) {
+      if (document.activeElement.nodeName !== 'TEXTAREA') {
+        getTextArea().focus();
+        getTextArea().selectionStart = getTextArea().innerHTML.length;
+      }
+      return;
+    }
+    if (document.activeElement.nodeName !== 'TEXTAREA') {
+      getTextArea().innerHTML += this.code;
+    }
+  }
+
+  releaseKey() {
+    this.classList.remove('pressed');
+  }
+
+  fillWithSymbol() {
+    if (!!this.alternativeCode) {
+      const alternative = document.createElement('sup');
+      alternative.innerText = this.alternativeCode;
+      this.appendChild(alternative);
+    }
+    const key = document.createElement('span');
+    key.innerText = this.displayValue ? this.displayValue : this.code;
+    this.appendChild(key);
+  }
+}
+
+customElements.define('virtual-key', VirtualKey, { extends: 'button' });
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
